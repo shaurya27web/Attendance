ae2d24c2efc516bf712064f4024ccc8a71865eaf
shaurya27web <shauryatiwari0011@gmail.com>
2026-04-21 23:33:59 +0530

    code

--- attendance-system/client/src/App.jsx
+++ attendance-system/client/src/App.jsx
@@ -24,7 +24,7 @@ export default function App() {
     <AuthProvider>
       <BrowserRouter>
         <Routes>
-          <Route path="/login" element={<Login />} />
+          
           <Route
             path="/dashboard"
             element={
